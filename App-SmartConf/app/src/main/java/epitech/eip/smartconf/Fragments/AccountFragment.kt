29425b5d30824e36a99f4951cd3a42cab3b3ec12
louis.dtr <louis.dautremer@epitech.eu>
2019-12-05 15:35:01 +0100

    [ADD] adding setting layout

--- App-SmartConf/app/src/main/java/epitech/eip/smartconf/Fragments/AccountFragment.kt
+++ App-SmartConf/app/src/main/java/epitech/eip/smartconf/Fragments/AccountFragment.kt
@@ -1,10 +1,23 @@
 package epitech.eip.smartconf.Fragments
 
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import epitech.eip.smartconf.Adapters.HomeAdapter
 import epitech.eip.smartconf.BaseClass.BaseFragment
 import epitech.eip.smartconf.R
+import kotlinx.android.synthetic.main.frag_account_layout.*
+import kotlinx.android.synthetic.main.frag_home_layout.*
 
 class AccountFragment: BaseFragment() {
     override fun getLayout(): Int { return R.layout.frag_account_layout }
     override fun shouldShowNavBar(): Boolean { return true }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        drop_down_option_menu.setOnClickListener {
+            placeFragment(AccountInfoFragment())
+        }
+    }
 }
---
+++ App-SmartConf/app/src/main/java/epitech/eip/smartconf/Fragments/AccountInfoFragment.kt
@@ -0,0 +1,10 @@
+package epitech.eip.smartconf.Fragments
+
+import epitech.eip.smartconf.BaseClass.BaseFragment
+import epitech.eip.smartconf.R
+
+class AccountInfoFragment: BaseFragment() {
+    override fun getLayout(): Int { return R.layout.frag_account_settings_layout }
+    override fun shouldShowNavBar(): Boolean { return true }
+
+}
