8c70ed339c86037f1de6f7ef2349170ad736fc90
louis.dtr <louis.dautremer@epitech.eu>
2019-12-05 15:50:13 +0100

    [CHANGE] account layout changing profile and profile_info layout

--- App-SmartConf/app/src/main/java/epitech/eip/smartconf/Fragments/AccountInfoFragment.kt
+++ App-SmartConf/app/src/main/java/epitech/eip/smartconf/Fragments/AccountInfoFragment.kt
@@ -6,5 +6,6 @@ import epitech.eip.smartconf.R
 class AccountInfoFragment: BaseFragment() {
     override fun getLayout(): Int { return R.layout.frag_account_settings_layout }
     override fun shouldShowNavBar(): Boolean { return true }
+    override fun setCustomActionBar(): Int { return R.layout.actionbar_return_layout }
 
 }
