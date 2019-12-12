b11eefc1f5c0591216f1685022088fb81ec8e8e0
Tyliam <silvinityliam@gmail.com>
2019-12-12 16:56:00 +0100

    [ADD] storage google storage

--- App-SmartConf/app/src/main/java/epitech/eip/smartconf/Fragments/MeetingDescFragment.kt
+++ App-SmartConf/app/src/main/java/epitech/eip/smartconf/Fragments/MeetingDescFragment.kt
@@ -2,22 +2,26 @@ package epitech.eip.smartconf.Fragments
 
 import android.media.MediaRecorder
 import android.os.Bundle
-import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import epitech.eip.smartconf.BaseClass.BaseFragment
 import epitech.eip.smartconf.R
-import epitech.eip.smartconf.cloudstorage.CloudStorageHelper
 import kotlinx.android.synthetic.main.frag_meetingdesc_layout.*
 import kotlinx.android.synthetic.main.fragelem_readytostart_layout.*
+import java.io.File
+import java.io.FileInputStream
 import java.io.IOException
 
 class MeetingDescFragment(private var active: Boolean): BaseFragment() {
-    private var cloudStorage: CloudStorageHelper = CloudStorageHelper()
     private var output: String? = null
     private var mediaRecorder: MediaRecorder? = null
     private var state: Boolean = false
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var storage: FirebaseStorage
 
     override fun getLayout(): Int { return R.layout.frag_meetingdesc_layout }
     override fun setCustomActionBar(): Int { return R.layout.actionbar_return_layout }
@@ -26,6 +30,7 @@ class MeetingDescFragment(private var active: Boolean): BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         frag_content.addView(loadActive().takeIf { active } ?: loadInactive())
+        mAuth = FirebaseAuth.getInstance()
 
         output = context?.getExternalFilesDir(null)?.absolutePath + "/recording.wav"
         //Environment.getExternalStorageDirectory().absolutePath + "/recording.wav"
@@ -45,7 +50,6 @@ class MeetingDescFragment(private var active: Boolean): BaseFragment() {
         button_stop_recording.setOnClickListener {
             stopRecording()
         }
-
     }
 
     private fun loadInactive(): View{
@@ -63,7 +67,6 @@ class MeetingDescFragment(private var active: Boolean): BaseFragment() {
             mediaRecorder?.prepare()
             mediaRecorder?.start()
             state = true
-            Toast.makeText(context, "Recording started!", Toast.LENGTH_SHORT).show()
         } catch (e: IllegalStateException) {
             e.printStackTrace()
         } catch (e: IOException) {
@@ -77,14 +80,22 @@ class MeetingDescFragment(private var active: Boolean): BaseFragment() {
                 mediaRecorder?.stop()
                 mediaRecorder?.release()
                 state = false
-                cloudStorage.uploadFile(output!!,"https://console.cloud.google.com/storage/browser/wavsoundfilebucket"
-                )
-                Toast.makeText(context, "Stop!", Toast.LENGTH_SHORT).show()
-            } catch (e: IOException) {
 
-            }
-        }else{
+                storage = FirebaseStorage.getInstance()
+                val storageRef = storage.reference.child("Meetings/Sounds/" + createToken())
+
+                val stream = FileInputStream(File(output))
+
+                storageRef.putStream(stream)
+                Toast.makeText(context, "Stop!", Toast.LENGTH_SHORT).show()
+            } catch (e: IOException) { }
+        } else{
             Toast.makeText(context, "You are not recording right now!", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun createToken(): String {
+        val chars = ('0'..'9').toList().toTypedArray() + ('a'..'z').toList().toTypedArray()
+        return (1..32).map { chars.random() }.joinToString { "" }
     }
 }
